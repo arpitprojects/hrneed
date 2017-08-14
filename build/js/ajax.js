a7daa8ec53933463a573f84aabd72aaded875890
Arpit Anand <mjarpitanand@gmail.com>
2017-08-14 17:16:02 +0530

    Commit - 14 Aug

---
+++ build/js/ajax.js
@@ -0,0 +1,36 @@
+function companysettings(){
+    $.ajax({
+        url : 'organisation/companysettings.html',
+        type : 'GET',
+        dataType : 'text',
+        success  : function(data){
+            
+            $('#get-ajax-content').html(data);
+        },
+        error: function(xhr, status, error) {
+            var err = eval("(" + xhr.responseText + ")");
+            alert(err.Message);
+        },
+        complete : function(com){
+            console.log('You arrived here '+com);
+        }
+    })
+}
+function departments(){
+    $.ajax({
+        url : 'organisation/departments.html',
+        type : 'GET',
+        dataType : 'text',
+        success  : function(data){
+            
+            $('#get-ajax-content').html(data);
+        },
+        error: function(xhr, status, error) {
+            var err = eval("(" + xhr.responseText + ")");
+            alert(err.Message);
+        },
+        complete : function(com){
+            console.log('You arrived here '+com);
+        }
+    })
+}
