c580eecd78d06716c008b4ce69db9f292dd0661d
Arpit Anand <mjarpitanand@gmail.com>
2017-08-29 16:29:40 +0530

    Commit 29 August

--- build/js/init.js
+++ build/js/init.js
@@ -362,3 +362,92 @@ altair_form_adv = {
 var container=document.querySelector('.tab-container')
 tabs(container);
 
+$(function() {
+    // help/faq
+    altair_help.init();
+});
+
+
+altair_help  = {
+    init: function() {
+        // variables
+        var $toggleAll_btn = $('#toggleAll'),
+            $help_accordion = $('.help_accordion');
+
+
+        UIkit.accordion($help_accordion, {
+            collapse: false,
+            showfirst: false
+        });
+
+        $toggleAll_btn.on('click', function(e) {
+            e.preventDefault();
+            $toggleAll_btn.velocity("transition.expandOut", {
+                duration: 280,
+                easing: easing_swiftOut,
+                begin: function() {
+                    if(!$help_accordion.hasClass('all_expanded')) {
+                        $help_accordion.addClass('all_expanded').find('.uk-accordion-title').not('.uk-active').trigger('click');
+                    } else {
+                        $help_accordion.removeClass('all_expanded').find('.uk-accordion-title.uk-active').trigger('click');
+                    }
+                },
+                complete: function() {
+                    $toggleAll_btn.velocity("transition.expandIn", {
+                        duration: 280,
+                        easing: easing_swiftOut,
+                        begin: function() {
+                            if(!$help_accordion.hasClass('all_expanded')) {
+                                $toggleAll_btn.html('&#xe8f2;');
+                            } else {
+                                $toggleAll_btn.html('&#xe8ee;');
+                            }
+                        }
+                    })
+                }
+            });
+
+        });
+    }
+};
+
+$(function() {
+    // copy user list to sidebar
+    altair_chat.copy_user_list();
+    // chat colors
+    altair_chat.change_colors();
+});
+
+var $chat = $('#chat'),
+    $user_list = $('#chat_user_list'),
+    $dropdown_chat_colors = $('#chat_colors');
+
+altair_chat = {
+    change_colors: function() {
+        $dropdown_chat_colors.find('a').click(function(e) {
+            e.preventDefault();
+            var thisColor = $(this).attr('data-chat-color');
+
+            $chat
+                .removeClass('chat_box_colors_a chat_box_colors_b chat_box_colors_c chat_box_colors_d')
+                .addClass(thisColor);
+
+            $(this)
+                .closest('li').addClass('uk-active')
+                .siblings('li').removeClass('uk-active');
+
+        })
+    },
+    copy_user_list: function() {
+        // hide secondary sidebar toggle btn for large screens
+        $sidebar_secondary_toggle.addClass('uk-hidden-large');
+
+        var user_list_sidebar = $user_list.clone();
+
+        user_list_sidebar.attr('id','notes_list_sidebar');
+
+        $sidebar_secondary
+            .find('.sidebar_secondary_wrapper').html(user_list_sidebar);
+
+    }
+};
