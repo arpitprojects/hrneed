f797251d3cd3e8259028a350737d3d0b3c97455b
Arpit Anand <mjarpitanand@gmail.com>
2017-09-07 13:00:23 +0530

    Angular binding starts

--- build/js/init.js
+++ build/js/init.js
@@ -516,16 +516,39 @@ function viewResume(){
     PDFObject.embed("../../demo/pdf.pdf");
 }
 
-setTimeout(function(){
-    Push.create("Hello world!", {
-        body: "How's it hangin'?",
-        icon: 'icon.png',
-        timeout: 4000,
-        onClick: function () {
-            window.focus();
-            this.close();
+$(function() {
+    // set width for scrum board container
+    altair_scrum_board.init();
+    // draggable tasks
+    altair_scrum_board.draggable_tasks();
+});
+
+altair_scrum_board = {
+    init: function() {
+        var $scrumBoard = $('#scrum_board'),
+            childWidth = $scrumBoard.children('div').width(),
+            childsCount = $scrumBoard.children('div').length;
+
+            $scrumBoard.width(childWidth * childsCount);
+    },
+    draggable_tasks: function() {
+
+        var drake = dragula($('.scrum_column > div').toArray());
+
+        var containers = drake.containers,
+            length = containers.length;
+
+        for (var i = 0; i < length; i++) {
+            $(containers[i]).addClass('dragula dragula-vertical');
         }
-    });
-} , 2000);
+
+        drake.on('drop', function(el, target, source, sibling) {
+            console.log(el);
+            console.log(target);
+            console.log(source);
+        })
+
+    }
+};
 
 /*End of the optional function */
